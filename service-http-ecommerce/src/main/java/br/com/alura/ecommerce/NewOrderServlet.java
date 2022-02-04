24fd2fb6a9d0eb2a640ab7f4d29c399b50570643
Liliane de Aquino <lilianelou@hotmail.com>
2022-02-04 06:55:49 -0300

    Add http-service

--- service-http-ecommerce/src/main/java/br/com/alura/ecommerce/NewOrderServlet.java
+++ service-http-ecommerce/src/main/java/br/com/alura/ecommerce/NewOrderServlet.java
@@ -1,16 +1,23 @@
 package br.com.alura.ecommerce;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
-public class NewOrderMain {
-
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
+public class NewOrderServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try (var orderDispatcher = new KafkaDispatcher<Order>()) {
             try (var emailDispatcher = new KafkaDispatcher<String>()) {
-                var email = Math.random()+"@email.com";
-                for (var i = 0; i < 10; i++) {
+                try {
+                    var email = Math.random()+"@email.com";
+
                     var orderId = UUID.randomUUID().toString();
                     var amount = new BigDecimal(Math.random() * 5000 + 1);
 
@@ -19,9 +26,12 @@ public class NewOrderMain {
 
                     var emailCode = "Thank you for your order! We are processing your order!";
                     emailDispatcher.send("ECOMMERCE_SEND_EMAIL", email, emailCode);
+                } catch (ExecutionException e) {
+                    throw new ServletException(e);
+                } catch (InterruptedException e) {
+                    throw new ServletException(e);
                 }
             }
         }
     }
-
 }
